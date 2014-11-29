ab786dae3a9f0394eb312eff04252d13d7a07d47
Anthony Sceresini <asceresini@gmail.com>
2014-11-29 17:22:58 +1100

    general deploy script

--- scripts/deploy_to_staging.sh
+++ scripts/deploy_to_staging.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-cd ./_artifact
-HEROKU_APP='site-4wardfabrication-staging'
-heroku apps | grep "^$HEROKU_APP\b" || heroku create --no-remote "$HEROKU_APP" --stack 'cedar'
-git init .
-git add --all
-MESSAGE="Site update at $(date '+%d/%m/%Y %H:%M:%S')"
-git commit -m "$MESSAGE"
-git push --force "git@heroku.com:$HEROKU_APP.git" 'master:master'
+./deploy.sh 'site-4wardfabrication-staging'
