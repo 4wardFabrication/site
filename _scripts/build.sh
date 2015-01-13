861831bfe03135ff91d4c2769cff76a503ec0326
Anthony Sceresini <asceresini@gmail.com>
2015-01-14 09:48:08 +1100

    adding jasmine ci to build stage

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 bundle install --retry 3
+bundle exec rake jasmine:ci
 bundle exec rake site:generate\[staging\]
 bundle exec rake site:test
 bundle exec rake artifact:generate\[staging\]
