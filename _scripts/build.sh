bf5b43e52b61ce3f3ac4862954795f5a6bda99a0
Anthony Sceresini <asceresini@gmail.com>
2015-01-15 18:57:55 +1100

    failing test correctly breaks the build

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-bundle install --retry 3
-bundle exec rake jasmine:ci
-bundle exec rake site:generate\[staging\]
-bundle exec rake site:test
-bundle exec rake artifact:generate\[staging\]
-bundle exec rake site:generate\[production\]
-bundle exec rake site:test
+bundle install --retry 3 &&
+bundle exec rake jasmine:ci &&
+bundle exec rake site:generate\[staging\] &&
+bundle exec rake site:test &&
+bundle exec rake artifact:generate\[staging\] &&
+bundle exec rake site:generate\[production\] &&
+bundle exec rake site:test &&
 bundle exec rake artifact:generate\[production\]
